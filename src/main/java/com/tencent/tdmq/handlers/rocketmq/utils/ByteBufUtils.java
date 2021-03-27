b2e999ff66aadd07b2fb4a7575d0ce53d9b64d1c
xiaolongran <xiaolongran@tencent.com>
2021-03-27 13:37:11 +0800

    add protocol handler util
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

---
+++ src/main/java/com/tencent/tdmq/handlers/rocketmq/utils/ByteBufUtils.java
@@ -0,0 +1,58 @@
+package com.tencent.tdmq.handlers.rocketmq.utils;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import io.netty.buffer.ByteBuf;
+import java.nio.ByteBuffer;
+import java.util.Base64;
+import org.apache.pulsar.common.api.proto.MessageMetadata;
+import org.apache.pulsar.common.api.proto.SingleMessageMetadata;
+
+/**
+ * @author xiaolongran@tencent.com
+ * @date 2021/3/27 1:35 下午
+ */
+public class ByteBufUtils {
+
+    public static ByteBuffer getKeyByteBuffer(SingleMessageMetadata messageMetadata) {
+        if (messageMetadata.hasOrderingKey()) {
+            return ByteBuffer.wrap(messageMetadata.getOrderingKey()).asReadOnlyBuffer();
+        }
+
+        if (messageMetadata.hasPartitionKey()) {
+            final String key = messageMetadata.getPartitionKey();
+            if (messageMetadata.hasPartitionKeyB64Encoded()) {
+                return ByteBuffer.wrap(Base64.getDecoder().decode(key)).asReadOnlyBuffer();
+            } else {
+                // for Base64 not encoded string, convert to UTF_8 chars
+                return ByteBuffer.wrap(key.getBytes(UTF_8));
+            }
+        } else {
+            return ByteBuffer.allocate(0);
+        }
+    }
+
+    public static ByteBuffer getKeyByteBuffer(MessageMetadata messageMetadata) {
+        if (messageMetadata.hasOrderingKey()) {
+            return ByteBuffer.wrap(messageMetadata.getOrderingKey()).asReadOnlyBuffer();
+        }
+
+        String key = messageMetadata.getPartitionKey();
+        if (messageMetadata.hasPartitionKeyB64Encoded()) {
+            return ByteBuffer.wrap(Base64.getDecoder().decode(key));
+        } else {
+            // for Base64 not encoded string, convert to UTF_8 chars
+            return ByteBuffer.wrap(key.getBytes(UTF_8));
+        }
+    }
+
+    public static ByteBuffer getNioBuffer(ByteBuf buffer) {
+        if (buffer.isDirect()) {
+            return buffer.nioBuffer();
+        }
+        final byte[] bytes = new byte[buffer.readableBytes()];
+        buffer.getBytes(buffer.readerIndex(), bytes);
+        return ByteBuffer.wrap(bytes);
+    }
+
+}
