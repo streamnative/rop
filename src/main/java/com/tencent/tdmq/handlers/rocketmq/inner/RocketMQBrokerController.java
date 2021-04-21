238117b1f99de394e90e63e061e61264cff1cff0
xiaolongran <xiaolongran@tencent.com>
2021-04-21 19:21:01 +0800

    Add license header for rop
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

--- src/main/java/com/tencent/tdmq/handlers/rocketmq/inner/RocketMQBrokerController.java
+++ src/main/java/com/tencent/tdmq/handlers/rocketmq/inner/RocketMQBrokerController.java
@@ -1,3 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tencent.tdmq.handlers.rocketmq.inner;
 
 import com.tencent.tdmq.handlers.rocketmq.RocketMQServiceConfiguration;
@@ -44,7 +58,6 @@ import org.apache.rocketmq.remoting.netty.NettyRequestProcessor;
 import org.apache.rocketmq.store.MessageArrivingListener;
 import org.apache.rocketmq.store.stats.BrokerStats;
 import org.apache.rocketmq.store.stats.BrokerStatsManager;
-
 
 @Data
 @Slf4j
--- src/main/java/com/tencent/tdmq/handlers/rocketmq/utils/ByteBufUtils.java
+++ src/main/java/com/tencent/tdmq/handlers/rocketmq/utils/ByteBufUtils.java
@@ -1,3 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tencent.tdmq.handlers.rocketmq.utils;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -8,10 +22,6 @@ import java.util.Base64;
 import org.apache.pulsar.common.api.proto.PulsarApi.MessageMetadata;
 import org.apache.pulsar.common.api.proto.PulsarApi.SingleMessageMetadata;
 
-/**
- * @author xiaolongran@tencent.com
- * @date 2021/3/27 1:35 下午
- */
 public class ByteBufUtils {
 
     public static ByteBuffer getKeyByteBuffer(SingleMessageMetadata messageMetadata) {
