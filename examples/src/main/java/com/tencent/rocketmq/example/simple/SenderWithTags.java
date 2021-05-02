0c4cdd42c7633429a41a162966c6ed036799ec7a
ronyhzhang <ronyhzhang@tencent.com>
2021-05-02 16:45:37 +0800

    when seek message and message has been marked deleted， continue to read next message

--- examples/src/main/java/com/tencent/rocketmq/example/simple/SenderWithTags.java
+++ examples/src/main/java/com/tencent/rocketmq/example/simple/SenderWithTags.java
@@ -4,17 +4,17 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 
-public class RoundRobinProducer {
-
+public class SenderWithTags {
     public static void main(String[] args) throws Exception {
 
         DefaultMQProducer producer = new DefaultMQProducer("test1|InstanceTest5", "pidTest");
         producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
+        String[] tags = {"tagA", "tagB"};
         for (int i = 1; i < 100; i++) {
-            Message message = new Message("topicTest", "tagTest", ("Hello world — " + i).getBytes());
+            Message message = new Message("topicTest", tags[i%2], ("Hello world — " + i).getBytes());
             try {
-                SendResult result = producer.send(message, 300000);
+                SendResult result = producer.send(message);
                 System.out.printf("Topic:%s send success, misId is:%s, queueId is: %s%n", message.getTopic(),
                         result.getMsgId(), result.getMessageQueue().getQueueId());
             } catch (Exception e) {
