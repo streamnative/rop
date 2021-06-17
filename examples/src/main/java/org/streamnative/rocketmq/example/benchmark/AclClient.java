f3af87d71f857fdf79665db728fac8c34000f72c
xiaolongran <xiaolongran@tencent.com>
2021-06-17 15:48:01 +0800

    Fix acl example for benchmark
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

--- examples/src/main/java/org/streamnative/rocketmq/example/benchmark/AclClient.java
+++ examples/src/main/java/org/streamnative/rocketmq/example/benchmark/AclClient.java
@@ -14,6 +14,7 @@
 
 package org.streamnative.rocketmq.example.benchmark;
 
+import org.apache.logging.log4j.util.Strings;
 import org.apache.rocketmq.acl.common.AclClientRPCHook;
 import org.apache.rocketmq.acl.common.SessionCredentials;
 import org.apache.rocketmq.remoting.RPCHook;
@@ -23,11 +24,10 @@ import org.apache.rocketmq.remoting.RPCHook;
  */
 public class AclClient {
 
-    private static final String ACL_ACCESS_KEY = "rocketmq2";
-
-    private static final String ACL_SECRET_KEY = "12345678";
+    private static final String ACL_ACCESS_KEY = "eyJrZXlJZCI6InB1bHNhci04xxxxxxxxxxUzI1NiJ9."
+            + "eyJzdWIiOiJwdWxxxxxx3QtMTExMTExIn0.cIsxxGtnuXxxxxxES-0WccDZjPEGUFzT-th-f6I";
 
     static RPCHook getAclRPCHook() {
-        return new AclClientRPCHook(new SessionCredentials(ACL_ACCESS_KEY, ACL_SECRET_KEY));
+        return new AclClientRPCHook(new SessionCredentials(ACL_ACCESS_KEY, Strings.EMPTY));
     }
 }
