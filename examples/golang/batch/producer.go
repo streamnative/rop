4c700781165d52074e74addb58ec7ccae15ad3d1
xiaolong ran <rxl@apache.org>
2021-09-07 15:39:38 +0800

    Add golang examples for repo (#59)
    * Add golang examples for repo
    
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>
    
    * Add golang examples for repo
    
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>
    
    * Add golang examples for repo
    
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>
    
    * Add golang examples for repo
    
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

---
+++ examples/golang/batch/producer.go
@@ -0,0 +1,56 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+)
+
+func main() {
+	p, _ := rocketmq.NewProducer(
+		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
+		producer.WithRetry(2),
+		producer.WithNamespace("test1|InstanceTest"),
+	)
+	err := p.Start()
+	if err != nil {
+		fmt.Printf("start producer error: %s", err.Error())
+		os.Exit(1)
+	}
+	var msgs []*primitive.Message
+	for i := 0; i < 10; i++ {
+		msgs = append(msgs, primitive.NewMessage("test_batch",
+			[]byte("Hello RocketMQ Go Client! num: "+strconv.Itoa(i))))
+	}
+
+	res, err := p.SendSync(context.Background(), msgs...)
+
+	if err != nil {
+		fmt.Printf("send message error: %s\n", err)
+	} else {
+		fmt.Printf("send message success: result=%s\n", res.String())
+	}
+	err = p.Shutdown()
+	if err != nil {
+		fmt.Printf("shutdown producer error: %s", err.Error())
+	}
+}
