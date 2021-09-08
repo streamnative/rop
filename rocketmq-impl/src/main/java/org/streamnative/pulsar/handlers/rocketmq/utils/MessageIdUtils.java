6965e3b9a089a1995f1a63f308ac84704ee42293
xiaolongran <xiaolongran@tencent.com>
2021-09-08 20:17:52 +0800

    Refactor messageID by Broker Entry Metadat
    Signed-off-by: xiaolongran <xiaolongran@tencent.com>

--- rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/utils/MessageIdUtils.java
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/utils/MessageIdUtils.java
@@ -15,10 +15,22 @@
 package org.streamnative.pulsar.handlers.rocketmq.utils;
 
 import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.mledger.AsyncCallbacks;
+import org.apache.bookkeeper.mledger.Entry;
+import org.apache.bookkeeper.mledger.ManagedLedger;
+import org.apache.bookkeeper.mledger.ManagedLedgerException;
+import org.apache.bookkeeper.mledger.impl.ManagedLedgerImpl;
 import org.apache.bookkeeper.mledger.impl.PositionImpl;
+import org.apache.pulsar.broker.intercept.ManagedLedgerInterceptorImpl;
 import org.apache.pulsar.client.api.MessageId;
 import org.apache.pulsar.client.impl.MessageIdImpl;
+import org.apache.pulsar.common.api.proto.BrokerEntryMetadata;
+import org.apache.pulsar.common.api.proto.MessageMetadata;
+import org.apache.pulsar.common.protocol.Commands;
 
 /**
  * MessageID utils class.
@@ -107,4 +119,94 @@ public class MessageIdUtils {
         }
         return false;
     }
+
+
+    public static long getCurrentOffset(ManagedLedger managedLedger) {
+        return ((ManagedLedgerInterceptorImpl) managedLedger.getManagedLedgerInterceptor()).getIndex();
+    }
+
+    public static long getLogEndOffset(ManagedLedger managedLedger) {
+        return getCurrentOffset(managedLedger) + 1;
+    }
+
+    public static long getPublishTime(final ByteBuf byteBuf) {
+        final int readerIndex = byteBuf.readerIndex();
+        Commands.skipBrokerEntryMetadataIfExist(byteBuf);
+        final MessageMetadata metadata = Commands.parseMessageMetadata(byteBuf);
+        byteBuf.readerIndex(readerIndex);
+        return metadata.getPublishTime();
+    }
+
+    public static CompletableFuture<Long> getOffsetOfPosition(
+            ManagedLedgerImpl managedLedger,
+            PositionImpl position,
+            boolean needCheckMore,
+            long timestamp) {
+        final CompletableFuture<Long> future = new CompletableFuture<>();
+        managedLedger.asyncReadEntry(position, new AsyncCallbacks.ReadEntryCallback() {
+            @Override
+            public void readEntryFailed(ManagedLedgerException exception, Object ctx) {
+                future.completeExceptionally(exception);
+            }
+
+            @Override
+            public void readEntryComplete(Entry entry, Object ctx) {
+                try {
+                    if (needCheckMore) {
+                        long offset = peekOffsetFromEntry(entry);
+                        final long publishTime = getPublishTime(entry.getDataBuffer());
+                        if (publishTime >= timestamp) {
+                            future.complete(offset);
+                        } else {
+                            future.complete(offset + 1);
+                        }
+                    } else {
+                        future.complete(peekBaseOffsetFromEntry(entry));
+                    }
+
+                } catch (Exception e) {
+                    future.completeExceptionally(e);
+                } finally {
+                    if (entry != null) {
+                        entry.release();
+                    }
+                }
+
+            }
+        }, null);
+        return future;
+    }
+
+    public static PositionImpl getPositionForOffset(ManagedLedger managedLedger, Long offset) {
+        try {
+            return (PositionImpl) managedLedger.asyncFindPosition(new OffsetSearchPredicate(offset)).get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("[{}] Failed to find position for offset {}", managedLedger.getName(), offset);
+            throw new RuntimeException(managedLedger.getName() + " failed to find position for offset " + offset);
+        }
+    }
+
+    public static BrokerEntryMetadata peekBrokerEntryMetadata(ByteBuf byteBuf) {
+        final int readerIndex = byteBuf.readerIndex();
+        BrokerEntryMetadata entryMetadata =
+                Commands.parseBrokerEntryMetadataIfExist(byteBuf);
+        byteBuf.readerIndex(readerIndex);
+        return entryMetadata;
+    }
+
+    public static long peekOffsetFromEntry(Entry entry) {
+        return Commands.peekBrokerEntryMetadataIfExist(entry.getDataBuffer()).getIndex();
+    }
+
+    public static long peekBaseOffsetFromEntry(Entry entry) {
+
+        return peekOffsetFromEntry(entry)
+                - Commands.peekMessageMetadata(entry.getDataBuffer(), null, 0)
+                .getNumMessagesInBatch() + 1;
+    }
+
+    public static long getMockOffset(long ledgerId, long entryId) {
+        return ledgerId + entryId;
+    }
+
 }
