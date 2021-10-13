022199139e47c708be773af96184138f383ac1a4
ronyhzhang <ronyhzhang@tencent.com>
2021-10-13 10:22:31 +0800

    fixed getMinOffset error log

--- rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/RopServerCnx.java
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/RopServerCnx.java
@@ -628,8 +628,8 @@ public class RopServerCnx extends ChannelInboundHandlerAdapter implements Pulsar
                     .getPulsarPersistentTopic(new ClientTopicName(topicName), pulsarPartitionId);
             maxOffset = this.brokerController.getConsumerOffsetManager()
                     .getMaxOffsetInQueue(new ClientTopicName(topicName), pulsarPartitionId);
-            minOffset = this.brokerController.getConsumerOffsetManager()
-                    .getMinOffsetInQueue(new ClientTopicName(topicName), pulsarPartitionId);
+            minOffset = queueOffset > 0 ? this.brokerController.getConsumerOffsetManager()
+                    .getMinOffsetInQueue(new ClientTopicName(topicName), pulsarPartitionId) : queueOffset;
         } catch (Exception e) {
             throw new RuntimeException();
         }
