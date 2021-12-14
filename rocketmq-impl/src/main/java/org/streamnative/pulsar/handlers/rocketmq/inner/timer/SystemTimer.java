9ae046d7560d4d268ffdddf213453a9a46f10aa7
hanmz <gunnhan@tencent.com>
2021-12-14 11:04:01 +0800

    Code optimization.

--- rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/timer/SystemTimer.java
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/timer/SystemTimer.java
@@ -21,8 +21,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Consumer;
 import javax.annotation.concurrent.ThreadSafe;
 import lombok.SneakyThrows;
