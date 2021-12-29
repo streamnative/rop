db56b84955ecc0d8149c7f31e5f5b584bcd05221
ronyhzhang <ronyhzhang@tencent.com>
2021-12-29 15:57:47 +0800

    add RoP metrics group

---
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/metrics/RopMetricsGroup.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.streamnative.pulsar.handlers.rocketmq.metrics;
+
+import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.core.Histogram;
+import com.yammer.metrics.core.Meter;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.core.Timer;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.util.Strings;
+import org.streamnative.pulsar.handlers.rocketmq.utils.Sanitizer;
+
+public class RopMetricsGroup {
+
+    public MetricName metricName(String name, Map<String, String> tags) {
+        Class<?> klass = this.getClass();
+        String pkg = (klass.getPackage() == null) ? "" : klass.getPackage().getName();
+        String simpleName = klass.getSimpleName().replaceAll("\\$$", "");
+        return explicitMetricName(pkg, simpleName, name, tags);
+    }
+
+
+    protected MetricName explicitMetricName(String group, String typeName, String name, Map<String, String> tags) {
+        StringBuilder nameBuilder = new StringBuilder();
+        nameBuilder.append(group);
+        nameBuilder.append(":type=");
+        nameBuilder.append(typeName);
+
+        if (Strings.isNotBlank(name)) {
+            nameBuilder.append(",name=");
+            nameBuilder.append(name);
+        }
+
+        String scope = toScope(tags);
+        String tagsName = toMBeanName(tags);
+        if (Strings.isNotBlank(tagsName)) {
+            nameBuilder.append(",").append(tagsName);
+        }
+
+        return new MetricName(group, typeName, name, scope, nameBuilder.toString());
+    }
+
+    public <T> Gauge<T> newGauge(String name, Gauge<T> metric, Map<String, String> tags) {
+        return RopYammerMetrics.defaultRegistry().newGauge(metricName(name, tags), metric);
+    }
+
+    public Meter newMeter(String name, String eventType, TimeUnit timeUnit, Map<String, String> tags) {
+        return RopYammerMetrics.defaultRegistry().newMeter(metricName(name, tags), eventType, timeUnit);
+    }
+
+    public Histogram newHistogram(String name, Boolean biased, Map<String, String> tags) {
+        return RopYammerMetrics.defaultRegistry().newHistogram(metricName(name, tags), biased);
+    }
+
+    public Timer newTimer(String name, TimeUnit durationUnit, TimeUnit rateUnit, Map<String, String> tags) {
+        return RopYammerMetrics.defaultRegistry().newTimer(metricName(name, tags), durationUnit, rateUnit);
+    }
+
+    public void removeMetric(String name, Map<String, String> tags) {
+        RopYammerMetrics.defaultRegistry().removeMetric(metricName(name, tags));
+    }
+
+    private String toMBeanName(Map<String, String> tags) {
+        List<Entry<String, String>> filteredTags = tags.entrySet().stream()
+                .filter(entry -> Strings.isNotBlank(entry.getValue()))
+                .collect(Collectors.toList());
+        if (!filteredTags.isEmpty()) {
+            String tagsString = filteredTags.stream()
+                    .map(entry -> "%s=%s".format(entry.getKey(), Sanitizer.jmxSanitize(entry.getValue())))
+                    .collect(Collectors.joining(","));
+            return tagsString;
+        }
+        return Strings.EMPTY;
+    }
+
+    private String toScope(Map<String, String> tags) {
+        List<Entry<String, String>> filteredTags = tags.entrySet().stream()
+                .filter(entry -> Strings.isNotBlank(entry.getValue()))
+                .sorted(Comparator.comparing(Entry::getKey))
+                .collect(Collectors.toList());
+
+        if (!filteredTags.isEmpty()) {
+            return filteredTags.stream()
+                    .map(entry -> "%s.%s".format(entry.getKey(), entry.getValue().replaceAll("\\.", "_")))
+                    .collect(Collectors.joining("."));
+        }
+        return Strings.EMPTY;
+    }
+}
---
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/metrics/RopYammerMetrics.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.streamnative.pulsar.handlers.rocketmq.metrics;
+
+import com.yammer.metrics.core.MetricsRegistry;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import org.streamnative.pulsar.handlers.rocketmq.inner.exception.RopRuntimeException;
+
+public class RopYammerMetrics {
+
+    public static final String METRICS_CONFIG_PREFIX = "metrics.jmx.";
+    public static final String EXCLUDE_CONFIG = METRICS_CONFIG_PREFIX + "exclude";
+    public static final String INCLUDE_CONFIG = METRICS_CONFIG_PREFIX + "include";
+    public static final String DEFAULT_INCLUDE = ".*";
+    public static final String DEFAULT_EXCLUDE = "";
+
+    public static final RopYammerMetrics INSTANCE = new RopYammerMetrics();
+
+    /**
+     * convenience method to replace {@link com.yammer.metrics.Metrics#defaultRegistry()}
+     */
+    public static MetricsRegistry defaultRegistry() {
+        return INSTANCE.metricsRegistry;
+    }
+
+    private final MetricsRegistry metricsRegistry = new MetricsRegistry();
+    private final FilteringJmxReporter jmxReporter = new FilteringJmxReporter(metricsRegistry,
+            metricName -> true);
+
+    private RopYammerMetrics() {
+        jmxReporter.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(jmxReporter::shutdown));
+    }
+
+    public void configure(Map<String, ?> configs) {
+        reconfigure(configs);
+    }
+
+    public void reconfigure(Map<String, ?> configs) {
+        Predicate<String> mBeanPredicate = compilePredicate(configs);
+        jmxReporter.updatePredicate(metricName -> mBeanPredicate.test(metricName.getMBeanName()));
+    }
+
+    public static Predicate<String> compilePredicate(Map<String, ?> configs) {
+        String include = (String) configs.get(INCLUDE_CONFIG);
+        String exclude = (String) configs.get(EXCLUDE_CONFIG);
+
+        if (include == null) {
+            include = DEFAULT_INCLUDE;
+        }
+
+        if (exclude == null) {
+            exclude = DEFAULT_EXCLUDE;
+        }
+
+        try {
+            Pattern includePattern = Pattern.compile(include);
+            Pattern excludePattern = Pattern.compile(exclude);
+
+            return s -> includePattern.matcher(s).matches()
+                    && !excludePattern.matcher(s).matches();
+        } catch (PatternSyntaxException e) {
+            throw new RopRuntimeException("JMX filter for configuration" + METRICS_CONFIG_PREFIX
+                    + ".(include/exclude) is not a valid regular expression");
+        }
+    }
+}
