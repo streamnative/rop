811207305f5e9da8145e528b621806634780055e
hanmz <gunnhan@tencent.com>
2021-12-29 18:17:56 +0800

    Fix born host incorrect.

--- rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/proxy/RopBrokerProxy.java
+++ rocketmq-impl/src/main/java/org/streamnative/pulsar/handlers/rocketmq/inner/proxy/RopBrokerProxy.java
@@ -338,7 +338,7 @@ public class RopBrokerProxy extends RocketMQRemoteServer implements AutoCloseabl
                     rmqTopic = new ClientTopicName(topic);
                     pulsarTopicName = rmqTopic.toPulsarTopicName();
                     isOwnedBroker = checkTopicOwnerBroker(cmd, pulsarTopicName, queueId);
-                    if (isOwnedBroker && cmd.getExtFields().containsKey(ROP_INNER_REMOTE_CLIENT_TAG)) {
+                    if (isOwnedBroker) {
                         log.trace("process owned broker send request[{}].", cmd);
                         super.processRequestCommand(ctx, cmd);
                     } else {
